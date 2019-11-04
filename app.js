ee4d576715c0a460d4751705b97b04e88535ccfe
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-04 22:28:45 +0100

    Iteerator example created

---
+++ app.js
@@ -0,0 +1,30 @@
+// // Iterator example
+
+// function nameIterator(names) {
+//   let nextIndex = 0
+
+//   return {
+//     next: function() {
+//       return nextIndex < names.length ?
+//       { value: names[nextIndex++], done: false } :
+//       { done: true }
+//     }
+//   }
+// }
+
+// // Create an array of names
+
+// const namesArr = ['Bart', 'Brad', 'Paul', 'Gabriel']
+
+// // Init iterator and pass in the names array
+// const names = nameIterator(namesArr)
+
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+
+// ----------------------------------------------------------------
+
+
+// Generator example
